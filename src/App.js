fd4a6e71d50bfefd1f417b132af009131b814349
jroopika <roopikajuluru04@gmail.com>
2025-02-19 23:36:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage'; // Import the Landing Page Component
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
